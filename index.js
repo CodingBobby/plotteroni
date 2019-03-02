582b7e7b786124a636e028d6420e8c720856a5fc
CodingBobby <contact@codingbobby.xyz>
2019-03-02 01:46:35 +0100

    module split
    Main change is that modules are being split into pieces. The index.js file is what the user puts his stuff in, pltrni.js holds the classes and methods the user needs and p5instance.js creates the actual canvas. Some more improvements made.

--- index.js
+++ index.js
@@ -1,137 +1,23 @@
-function setup() {
-   createCanvas(cvs.w, cvs.h)
-}
-function draw() {
-   background(255)
-
-   // axis
-   stroke(10)
-   strokeWeight(1)
-   line(0, center.y, cvs.w, center.y)
-   line(center.x, cvs.h, center.x, 0)
-
-   // tics
-   for(var i=tics.y.n; i<=tics.y.p; i++) {
-      if(i == 0) i++
-
-      stroke(10)
-      strokeWeight(1)
-      line(center.x-5, center.y-i*one.y, center.x+5, center.y-i*one.y)
-
-      noStroke()
-      fill(1)
-      text(i.toString(), center.x+10, center.y-i*one.y +5)
-   }
-   for(var i=tics.x.n; i<=tics.x.p; i++) {
-      if(i == 0) i++
-
-      stroke(10)
-      strokeWeight(1)
-      line(center.x-i*one.x, center.y-5, center.x-i*one.x, center.y+5)
-
-      noStroke()
-      fill(1)
-      j = -i
-      text(j.toString(), center.x-i*one.x-2.5*j.toString().length, center.y+20)
-   }
-
-   // plot
-   noFill()
-   strokeWeight(1.5)
-   for(var f in fig) {
-      let cc = f
-      if(f >= colors.length) {
-         cc = f % colors.length
-      }
-      stroke(colors[cc].r, colors[cc].g, colors[cc].b)
-      beginShape()
-      for(var i in fig[f]) {
-         vertex(fig[f][i].x, fig[f][i].y)
-      }
-      endShape()
-   }
+const options = {
+    height: 500,
+    width: 500,
+    scale: 0.8
 }
 
-let colors = [{
-   r: 25, g: 25, b: 200
-}, {
-   r: 25, g: 200, b: 25
-}, {
-   r: 200, g: 25, b: 25
-}, {
-   r: 100, g: 125, b: 25
-}, {
-   r: 50, g: 75, b: 100
-}, {
-   r: 110, g: 12, b: 190
-}, {
-   r: 200, g: 110, b: 45
-}]
+const plot = new Plotteroni(options)
 
-let cvs = {
-   h: 600,
-   w: 900
+let sin = function(x) {
+    return Math.sin(5*x) + 4
 }
 
-let center = {
-   x: cvs.w/2,
-   y: cvs.h/2
+let cool = function(x) {
+    return Math.log(x + 4) * Math.sin(x)
 }
 
-let fig = []
-let res = 1
-let scale = {
-   x: 1,
-   y: 1
+let cooler = function(x) {
+    return Math.asin(Math.sin(x - 1.75) - Math.cos(x)) - 3.7
 }
 
-let one = {
-   x: scale.x * cvs.w / 10,
-   y: scale.y * cvs.w / 10
-}
+plot.add(sin, cool, cooler)
 
-let yMax = {
-   p: center.y/one.y +10,
-   n: -(cvs.h - center.y)/one.y -10
-}
-
-let tics = {
-   x: {
-      p: Math.floor(center.x/one.x),
-      n: Math.floor((center.x - cvs.w)/one.x)
-   },
-   y: {
-      p: Math.floor(center.y/one.y),
-      n: -Math.floor((cvs.h - center.y)/one.y)
-   }
-} 
-function k(x) {
-   return Math.sin(x) * Math.log(x)**2
-}
-
-let functionArray = []
-
-functionArray.push(k)
-
-let counter = 0
-
-functionArray.forEach(func => {
-   fig.push([])
-   for(var i=-center.x; i<=(cvs.w-center.x); i++) {
-      if(i % res == 0) {
-         let x = i / one.x
-         let y = func(x)
-         if(y > yMax.p) {
-            y = yMax.p
-         } else if(y < yMax.n) {
-            y = yMax.n
-         }
-         let obj = {
-            x: x * one.x + center.x,
-            y: center.y - y * one.y
-         }
-         fig[counter].push(obj)
-      }
-   }
-   counter++
-})
+plot.draw()
---
+++ pltrni.js
@@ -0,0 +1,57 @@
+class Plotteroni {
+    constructor(options) {
+        if(typeof options != 'object') {
+            return console.error('invalid options')
+        } else {
+            for(var i in options) {
+                let is = false
+                for(var j in valOpts) {
+                    if(valOpts[j].name == i) {
+                        for(var m in valOpts[j].type) {
+                            if(typeof options[i] == valOpts[j].type[m]) {
+                                is = true
+                            }
+                        }
+                    }
+                }
+                if(!is) return console.error('invalid options')
+            }
+        }
+        this.functions = []
+        this.options = options
+    }
+
+    add(...funcs) {
+        for(var i in funcs) {
+            let func = funcs[i]
+            if(typeof func != 'function') {
+                return console.error('invalid function')
+            }
+            this.functions.push(func)
+        }
+    }
+
+    draw() {
+        this.load()
+    }
+
+    load() {
+        var script1 = document.createElement("script")
+        script1.src = 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js'
+        document.head.appendChild(script1)
+        let script2 = document.createElement("script")
+        script2.src = 'p5instance.js'
+        document.body.appendChild(script2)
+    }
+}
+
+let valOpts = [{
+    name: 'width',
+    type: ['number']
+}, {
+    name: 'height',
+    type: ['number']
+}, {
+    name: 'scale',
+    type: ['number', 'object']
+}]
